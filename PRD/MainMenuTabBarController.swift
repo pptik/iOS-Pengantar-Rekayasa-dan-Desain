e16410d9c0e1219c29519d19d81b7e9495d5fcfa
emilhamep <emilhamep@gmail.com>
2017-02-27 18:19:32 +0700

    Penerapan Layout Tab

--- PRD/MainMenuTabBarController.swift
+++ PRD/MainMenuTabBarController.swift
@@ -1,33 +1,29 @@
 //
-//  LoginViewController.swift
+//  MainMenuTabBarController.swift
 //  PRD
 //
-//  Created by Ilham on 2/26/17.
+//  Created by Ilham on 2/27/17.
 //  Copyright © 2017 Ilham. All rights reserved.
 //
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class MainMenuTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-        //Warna untuk navigation bar
-        navigationController?.navigationBar.barTintColor = UIColor(red: 100/255.0, green: 181/255.0, blue: 246/255.0, alpha: 1.0)
         
-        //Warna untuk tulisan dinavigation bar
-        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
-        
-        
-        
-        navigationController?.navigationBar.tintColor = .white
-        
-        //Menyembunyikan tombol kembali
+        //Kustomisasi tombol kembali
         self.navigationItem.setHidesBackButton(true, animated:true)
-
-
+        //self.navigationController?.navigationBar.backItem?.title = "Keluar"
+        
+        //Memberikan tombol berupa icon
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,10 +31,9 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func Masuk(_ sender: UIButton) {
-        print("Tombol masuk ditekan")
+    func debugs(){
+        print("debug")
     }
-    
 
     /*
     // MARK: - Navigation
